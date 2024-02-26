260190d0fd9106d8faee557b461bc4348211bad7
Jisun Park <148641571+jisunipark@users.noreply.github.com>
2024-02-26 09:24:00 +0900

    지선 | 카운터 기능 구현
    * Chore: 프로젝트 초기 세팅
    
    * Chore: 불필요한 코드 삭제
    
    * Chore: styled-components 설치
    
    * Feat: 카운터 기능 구현
    
    * Deisgn: 스타일 적용

---
+++ src/components/Counter.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Buttons, StyledCounter } from "./CounterStyle";
+
+export default function Counter() {
+  const [number, setNubmer] = useState(0);
+
+  const handlePlus = () => {
+    if (number === 10) return;
+    setNubmer((prev) => prev + 1);
+  };
+
+  const handleMinus = () => {
+    if (number === 0) return;
+    setNubmer((prev) => prev - 1);
+  };
+
+  return (
+    <StyledCounter>
+      <div className="count">{number}</div>
+      <Buttons>
+        <button type="button" onClick={handleMinus}>
+          -
+        </button>
+        <button type="button" onClick={handlePlus}>
+          +
+        </button>
+      </Buttons>
+    </StyledCounter>
+  );
+}
