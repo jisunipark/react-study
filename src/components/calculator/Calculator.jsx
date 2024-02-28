16242d2f0e23778a32d2b21250c3484fb41008c9
Jisun Park <jisunpark.t@gmail.com>
2024-02-28 10:11:57 +0900

    Implement calculation of single digit numbers

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -15,45 +15,78 @@ const Calculated = styled.div`
 
 export default function Calculator() {
   const [number, setNumber] = useState(0);
+  const [hold, setHold] = useState();
+  const [operator, setOperator] = useState("");
 
   const handleNumberClick = (e) => {
-    const nextNum = e.target.textContent;
-    setNumber(nextNum);
+    const nextNum = Number(e.target.textContent);
+    if (hold) {
+      switch (operator) {
+        case "+":
+          setNumber(hold + nextNum);
+          break;
+        case "-":
+          setNumber(hold - nextNum);
+          break;
+        case "x":
+          setNumber(hold * nextNum);
+          break;
+        case "/":
+          setNumber(hold / nextNum);
+          break;
+        default:
+          break;
+      }
+    } else {
+      setNumber(nextNum);
+    }
+  };
+
+  const handleOperatorClick = (e) => {
+    setHold(number);
+    setOperator(e.target.textContent);
   };
 
   const handleResetClick = () => {
     setNumber(0);
+    setHold();
+    setOperator("");
   };
 
   return (
-    <Container>
-      <Calculated>{number}</Calculated>
-      <div>
-        <Button onClick={handleResetClick}>AC</Button>
-        <Button>/</Button>
-      </div>
-      <div>
-        <Button onClick={handleNumberClick}>8</Button>
-        <Button onClick={handleNumberClick}>7</Button>
-        <Button onClick={handleNumberClick}>9</Button>
-        <Button>x</Button>
-      </div>
-      <div>
-        <Button onClick={handleNumberClick}>4</Button>
-        <Button onClick={handleNumberClick}>5</Button>
-        <Button onClick={handleNumberClick}>6</Button>
-        <Button>-</Button>
-      </div>
-      <div>
-        <Button onClick={handleNumberClick}>1</Button>
-        <Button onClick={handleNumberClick}>2</Button>
-        <Button onClick={handleNumberClick}>3</Button>
-        <Button>+</Button>
-      </div>
-      <div>
-        <Button onClick={handleNumberClick}>0</Button>
-        <Button>=</Button>
-      </div>
-    </Container>
+    <>
+      <Container>
+        <Calculated>{number}</Calculated>
+        <div>
+          <Button onClick={handleResetClick}>AC</Button>
+          <Button onClick={handleOperatorClick}>/</Button>
+        </div>
+        <div>
+          <Button onClick={handleNumberClick}>8</Button>
+          <Button onClick={handleNumberClick}>7</Button>
+          <Button onClick={handleNumberClick}>9</Button>
+          <Button onClick={handleOperatorClick}>x</Button>
+        </div>
+        <div>
+          <Button onClick={handleNumberClick}>4</Button>
+          <Button onClick={handleNumberClick}>5</Button>
+          <Button onClick={handleNumberClick}>6</Button>
+          <Button onClick={handleOperatorClick}>-</Button>
+        </div>
+        <div>
+          <Button onClick={handleNumberClick}>1</Button>
+          <Button onClick={handleNumberClick}>2</Button>
+          <Button onClick={handleNumberClick}>3</Button>
+          <Button onClick={handleOperatorClick}>+</Button>
+        </div>
+        <div>
+          <Button onClick={handleNumberClick}>0</Button>
+          <Button onClick={handleOperatorClick}>=</Button>
+        </div>
+        <p>---------------</p>
+        <p>hold: {hold}</p>
+        <p>operator: {operator}</p>
+      </Container>
+    </>
   );
 }
