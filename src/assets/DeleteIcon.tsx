9c3d74853a4a188b4bd74974d4e817a1ed50a894
Jisun Park <jisunpark.t@gmail.com>
2024-04-18 00:21:00 +0900

    Design: Add and edit Icons

--- src/assets/DeleteIcon.tsx
+++ src/assets/DeleteIcon.tsx
@@ -1,8 +1,18 @@
 export default function DeleteIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
-      <path d="M2 15.0815L14.7279 2.35355" stroke="#252525" strokeWidth="3" strokeLinecap="round" />
-      <path d="M15.0815 14.7279L2.35362 2" stroke="#252525" strokeWidth="3" strokeLinecap="round" />
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+      <path
+        d="M2.35297 17.7429L17.327 2.76889"
+        stroke="#252525"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
+      <path
+        d="M17.743 17.327L2.76895 2.35294"
+        stroke="#252525"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
     </svg>
   );
 }
---
+++ src/assets/PlusIcon.tsx
@@ -0,0 +1,13 @@
+export default function PlusIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+      <path d="M1.81818 10L18.1818 10" stroke="#BBBBBB" stroke-width="3" stroke-linecap="round" />
+      <path
+        d="M9.91211 18.2545L9.91211 1.89088"
+        stroke="#BBBBBB"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+}
