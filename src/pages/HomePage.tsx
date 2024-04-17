553f1353459d733597a82ff34ce55dc075fab74b
Jisun Park <jisunpark.t@gmail.com>
2024-04-17 14:58:44 +0900

    Feat: Add links to each components at Homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,17 @@
+import { Link } from 'react-router-dom';
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <div>
+        <Link to="/calculator">Calculator</Link>
+      </div>
+      <div>
+        <Link to="/counter">Counter</Link>
+      </div>
+      <div>
+        <Link to="/todo">Todo List</Link>
+      </div>
+    </div>
+  );
 }
