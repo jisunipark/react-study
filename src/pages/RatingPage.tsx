ca6742fe9d2f387f025eddff9ae59c85afee58d3
Jisun Park <jisunpark.t@gmail.com>
2024-05-07 20:09:42 +0900

    Design: Set background color

--- src/pages/RatingPage.tsx
+++ src/pages/RatingPage.tsx
@@ -1,3 +1,14 @@
+import styled from 'styled-components';
+
 export default function RatingPage() {
-  return <div>rating</div>;
+  return <StyledPage></StyledPage>;
 }
+
+const StyledPage = styled.div`
+  display: flex;
+  width: 1512px;
+  height: 982px;
+  padding: 271px 0px;
+  justify-content: center;
+  align-items: center;
+`;
