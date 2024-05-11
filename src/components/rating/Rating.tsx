62e0d3067ac1d106d569cf50a21c627527a21bb8
Jisun Park <jisunpark.t@gmail.com>
2024-05-11 18:55:17 +0900

    Feat: Change star color on mouse over

--- src/components/rating/Rating.tsx
+++ src/components/rating/Rating.tsx
@@ -5,34 +5,46 @@ import { RATING_MESSAGE } from './constants';
 interface StarProps {
   rating: number;
   selected: boolean;
-  onClick: (key: number) => void;
+  hoverValue: number;
+  onClick: (rating: number) => void;
+  onHover: (rating: number) => void;
 }
 
 interface RatingProps {
   value: number;
-  onClick: (key: number) => void;
+  hoverValue: number;
+  onClick: (rating: number) => void;
+  onHover: (rating: number) => void;
 }
 
 const RATINGS = [1, 2, 3, 4, 5];
 
-function Star({ rating, selected, onClick }: StarProps) {
-  const fill = selected ? '#FB861D' : '#E8EAF5';
+function Star({ rating, selected, hoverValue, onClick, onHover }: StarProps) {
+  const fill = selected ? '#FB861D' : hoverValue >= rating ? '#FFD97B' : '#E8EAF5';
 
   const handleOnClick = () => onClick(rating);
+  const handleOnHover = () => onHover(rating);
 
   return (
-    <button type="button" onClick={handleOnClick}>
+    <button type="button" onClick={handleOnClick} onMouseOver={handleOnHover}>
       <StarIcon fill={fill} />
     </button>
   );
 }
 
-export default function Rating({ value, onClick }: RatingProps) {
+export default function Rating({ value, hoverValue, onClick, onHover }: RatingProps) {
   return (
     <StyledRating /* $size={size} */>
       <div className="rating-stars">
         {RATINGS.map((rating) => (
-          <Star key={rating} rating={rating} selected={value >= rating} onClick={onClick} />
+          <Star
+            key={rating}
+            rating={rating}
+            selected={value >= rating}
+            hoverValue={hoverValue}
+            onClick={onClick}
+            onHover={onHover}
+          />
         ))}
       </div>
       <span className="rating-comment">{RATING_MESSAGE[value]}</span>
--- src/components/rating/RatingInput.tsx
+++ src/components/rating/RatingInput.tsx
@@ -3,10 +3,17 @@ import Rating from './Rating';
 
 export default function RatingInput() {
   const [value, setValue] = useState(0);
+  const [hoverValue, setHoverValue] = useState(0);
 
-  const handleOnClick = (value: number) => {
-    setValue(value);
+  const handleOnClick = (rating: number) => {
+    setValue(rating);
   };
 
-  return <Rating value={value} onClick={handleOnClick} />;
+  const handleOnHover = (rating: number) => {
+    setHoverValue(rating);
+  };
+
+  return (
+    <Rating value={value} hoverValue={hoverValue} onClick={handleOnClick} onHover={handleOnHover} />
+  );
 }
