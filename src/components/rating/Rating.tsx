0c74890a6dffa59f08395a04ab895cf911864596
Jisun Park <jisunpark.t@gmail.com>
2024-05-11 18:29:47 +0900

    Feat: Show proper stars and message depending on the rating value

--- src/components/rating/Rating.tsx
+++ src/components/rating/Rating.tsx
@@ -1,21 +1,31 @@
 import StarIcon from '../../assets/StarIcon';
 import { StyledRating } from './RatingStyle';
+import { RATING_MESSAGE } from './constants';
 
-interface RatingProps {
-  size: 'lg' | 'sm';
+interface StarProps {
+  selected: boolean;
 }
 
-export default function Rating({ size }: RatingProps) {
+interface RatingProps {
+  value?: number /* TODO 옵셔널 없애기 */;
+}
+
+const RATINGS = [1, 2, 3, 4, 5];
+
+function Star({ selected }: StarProps) {
+  const fill = selected ? '#FB861D' : '#E8EAF5';
+  return <StarIcon fill={fill} />;
+}
+
+export default function Rating({ value = 3 }: RatingProps) {
   return (
-    <StyledRating $size={size}>
+    <StyledRating /* $size={size} */>
       <div className="rating-stars">
-        <StarIcon size={size} />
-        <StarIcon size={size} />
-        <StarIcon size={size} />
-        <StarIcon size={size} />
-        <StarIcon size={size} />
+        {RATINGS.map((rating) => (
+          <Star key={rating} selected={value >= rating} />
+        ))}
       </div>
-      <span className="rating-comment">Select an option</span>
+      <span className="rating-comment">{RATING_MESSAGE[value]}</span>
     </StyledRating>
   );
 }
---
+++ src/components/rating/constants.ts
@@ -0,0 +1,14 @@
+export const STATUS_COLOR: { none: string; hovered: string; selected: string } = {
+  none: '#E8EAF5',
+  hovered: '#FFD97B',
+  selected: '#FB861D',
+};
+
+export const RATING_MESSAGE: { [rating: number]: string } = {
+  0: 'Select an option',
+  1: 'Not quite...',
+  2: 'Could be better',
+  3: 'Decent',
+  4: 'Pretty good!',
+  5: 'Super cool!',
+};
